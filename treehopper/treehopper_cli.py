912b7d882921f1d3d36b7bf46915e49f34597e7a
Nitin Kumar Karma <nitinkumarkarma@Nitins-MacBook-Air.local>
2025-11-22 01:41:35 +0530

    modified the cleanup template and added treehopper cleaner integrated to cli

--- treehopper/treehopper_cli.py
+++ treehopper/treehopper_cli.py
@@ -776,7 +776,7 @@ def main() -> None:
             sys.exit(1)
         push_file(sys.argv[2], sys.argv[3])
     elif cmd == "clean":
-        from treehopper_cleaner import main as clean_main
+        from .treehopper_cleaner import main as clean_main
 
         clean_main()
     else:
